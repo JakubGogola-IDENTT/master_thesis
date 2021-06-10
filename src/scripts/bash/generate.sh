2be39dba8a83221e5d42733e6834f1e552efdfd8
JakubGogola <jakubgogola97@gmail.com>
2021-06-10 16:18:24 +0200

    code: some changes

--- src/scripts/bash/generate.sh
+++ src/scripts/bash/generate.sh
@@ -4,4 +4,4 @@ readonly scriptsDir=$( cd "$(dirname "$0")" ; pwd -P )
 readonly tmpDir="$scriptsDir"/../../tmp
 readonly juliaScriptsDir="$scriptsDir"/../julia
 
-julia "$juliaScriptsDir/generate_data.jl" --min 5 --max 23 --schemas-dir "$tmpDir/schemas" --models-dir "$tmpDir/models" --scripts-dir "$juliaScriptsDir"
+julia "$juliaScriptsDir/generate_data.jl" --min 2 --max 23 --schemas-dir "$tmpDir/schemas" --models-dir "$tmpDir/models" --scripts-dir "$juliaScriptsDir"
